8deed954086c7a23af2922eba88f0119136b46ea
Thomas Dupoiron <tdupoiron@github.com>
2022-10-04 14:09:19 +0200

    Init maven project

---
+++ ghas-java-alerts/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.tdupoiron.github.ghas_java_alerts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
